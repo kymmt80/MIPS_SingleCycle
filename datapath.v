81eedb1c76233e25e8aa7e832806029de6eeae76
Kimiya Mohammadtaheri <63723127+kymmt80@users.noreply.github.com>
2021-05-10 12:02:15 +0430

    add jump to datapath

--- datapath.v
+++ datapath.v
@@ -1,5 +1,5 @@
-module datapath(input clk,rst,Reg_Write,memWrite,memRead,RegDst,ALUsrc,memToReg,PCsrc,input [2:0]ALU_control,output [31:0]inst,output Zero);
-    wire [31:0]writeData,regAddress,instAddress,readDataMem,Write_Data,Read_Data1, Read_Data2,ALUin1,ALUin2,ALUout,PCsrcIn,addIn,JumpIn;
+module datapath(input clk,rst,Reg_Write,memWrite,memRead,RegDst,ALUsrc,memToReg,PCsrc,Jmp,input [2:0]ALU_control,output [31:0]inst,output Zero);
+    wire [31:0]writeData,regAddress,instAddress,readDataMem,Write_Data,Read_Data1, Read_Data2,ALUin1,ALUin2,ALUout,PCsrcIn,addIn,branchIn,notJmp;
     wire [4:0]Read_Reg1, Read_Reg2, Write_Reg;
 
     register PC(
@@ -16,17 +16,23 @@ module datapath(input clk,rst,Reg_Write,memWrite,memRead,RegDst,ALUsrc,memToReg,
         .res(addIn)
     );
 
-    adder jumpAdd(
+    adder branchAdd(
         .a(addIn),
         .b({{14{inst[15]}},inst[15:0],2'b00}),
-        .res(JumpIn)
+        .res(branchIn)
     );
 
-    mux2nton PCmux(
+    mux2nton branchmux(
         .a(addIn),
-        .b(JumpIn),
-        .o(PCsrcIn),
+        .b(branchIn),
+        .o(notJmp),
         .s(PCsrc)
+    );
+    mux2nton Jmpmux(
+        .a(notJmp),
+        .b({instAddress[31:28],inst[25:0],2'b00}),
+        .o(PCsrcIn),
+        .s(Jmp)
     );
 
     inst_mem im(
